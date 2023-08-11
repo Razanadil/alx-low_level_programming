e0f2d5c07a37d7a04384ec1853539c919cdcbe91
Razanadil <razan.adil3@gmail.com>
2023-08-10 19:16:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main -  intry piont
  * return: 0 (succes)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 	}
 
