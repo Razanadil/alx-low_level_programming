41e3759671d95e59463a7b76f3f44b2d6af495cd
Razanadil <razan.adil3@gmail.com>
2023-08-12 11:40:49 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ /**
+ * main - Entry point
+ * Description: print the hexadecimal  
+ * Return: o (success)
+ */
+int main(void)
+{
+Char n;
+for (n = 0; n <= 9; n++)
+putchar(n);
+for (ch = 'a'; ch <= 'f'; ch++)
+putchar(n);
+putchar('\n');
+return (0);
+}
+
