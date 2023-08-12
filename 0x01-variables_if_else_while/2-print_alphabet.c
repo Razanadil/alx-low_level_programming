de85e33bbf8bbe09e8ea492d1caec1ea9beb8816
Razanadil <razan.adil3@gmail.com>
2023-08-12 06:39:31 -0700

    the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ /**
+ * main - Entry point
+ * Description: print the alphabet in lowercase
+ * Return: o (success)
+ */
+int main (main)
+{
+    char ch;
+     for (ch = 'a' , ch <= 'z' , ch++ )
+          putchar(ch);
+     putchar('\n');
+                   return(0);
+    }
+
