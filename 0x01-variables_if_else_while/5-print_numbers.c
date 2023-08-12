e89f5582b7fda32afe1651b2d31bf64cb36c62ff
Razanadil <razan.adil3@gmail.com>
2023-08-12 07:24:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: print digits of base 10 from 0
+ * Return: o (success)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <=  9; n++)
+printf("%d\n", n);  
+
+return (0);
+}
+
