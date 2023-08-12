355b22af5c3a9401321d7db50946c29630066729
Razanadil <razan.adil3@gmail.com>
2023-08-12 11:50:53 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
  /**
  * main - Entry point
- * Description: print the hexadecimal  
+ * Description: print numbers of base 16   
  * Return: o (success)
  */
 int main(void)
 {
 char n;
-for (n = 0; n <= 9; n++)
+for (n = '0'; n <= '9'; n++)
 putchar(n);
 for (n = 'a'; n <= 'f'; n++)
 putchar(n);
