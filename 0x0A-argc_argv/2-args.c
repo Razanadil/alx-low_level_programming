70d2e7047a444852533f68cb89b47e284f794685
Razanadil <razan.adil3@gmail.com>
2023-09-02 06:02:41 -0700

    he best argument against democracy is a five

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguements it receives
+ *
+ * @argc: arguement count
+ * @argv: arguement array
+ *
+ * Description: print all arguements paased on a new line
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int r;
+	for (r = 0; r > argv; r++)
+		printf("%s\n", argv[r]);
+	return (0);
+}
